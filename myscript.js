c0a08c39d73a32b56f802575c27982969ccd1b3f
suseendran93 <ssndrn@gmail.com>
2019-03-13 22:43:16 +0530

    Added homepage

---
+++ myscript.js
@@ -0,0 +1,22 @@
+	window.onload=hideInfo();
+	
+	function showInfo(){
+		hideInfo();
+		var val=document.getElementById("dropdown").value;
+			 if(val == "RPG")
+				document.getElementById("RPG").style.display='block';
+			else if(val == "FPS")
+				document.getElementById("FPS").style.display='block';
+			else if(val == "Racing")
+				document.getElementById("Racing").style.display='block';
+			else if(val == "Free to play")
+				document.getElementById("Free to play").style.display='block';
+			}
+
+	function hideInfo(){
+
+				document.getElementById("RPG").style.display='none';
+				document.getElementById("FPS").style.display='none';
+				document.getElementById("Racing").style.display='none';
+				document.getElementById("Free to play").style.display='none';
+					}
